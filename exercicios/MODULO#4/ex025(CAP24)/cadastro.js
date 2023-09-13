02236de9bf8d6bfa55a9bc4c5f4a9db6826651b6
Luiz Isaque <83297869+LUIZ919117215@users.noreply.github.com>
2023-09-13 13:59:48 -0300

    j.s

---
+++ exercicios/MODULO#4/ex025(CAP24)/cadastro.js
@@ -0,0 +1,36 @@
+Osdados foram enviados!
+Aprenda PHP para saber o que fazer com eles.
+
+
+
+
+
+
+
+
+
+
+
+console.alert()
+function name(resultado1) {
+    const carro = "rodas"
+    let moto = 4
+    var placa = true
+
+    if () {
+
+    } else {
+
+    }
+
+    while () {
+
+    }
+    for (var i = 0;i > 10;i++) {
+
+    }
+}
+
+
+
+
